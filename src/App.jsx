7684e6f1cfd435d5dcda81ec023470b5edd33d23
RobertMaksymow <114484269+RobertMaksymow@users.noreply.github.com>
2025-03-09 22:12:28 +0000

    Basic explanation of styling and useState in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
+import React, { useState } from "react";
+
 const Card = ({ title }) => {
-  return <h2>{title}</h2>;
+  const [hasLiked, setHasLiked] = useState(false);
+  return (
+    <div className="card">
+      <h2>{title}</h2>
+      <button onClick={() => setHasLiked(!hasLiked)}>
+        {hasLiked ? "<3" : "Like"}
+      </button>
+    </div>
+  );
 };
 
 const App = () => {
   return (
-    <>
-      <h2>hello</h2>
+    <div className="card-container">
       <Card title="Movie 1" />
       <Card title="Movie 2" />
       <Card title="Movie 3" />
-    </>
+    </div>
   );
 };
 
