ffca66f84cb0806a615e49be4a72ecb01fd5129d
RobertMaksymow <114484269+RobertMaksymow@users.noreply.github.com>
2025-03-19 20:51:49 +0000

    Project cleaned. Starting movie app. Time 51min.

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,16 @@
-import React, { useEffect, useState } from "react";
-
-const Card = ({ title }) => {
-  const [hasLiked, setHasLiked] = useState(false);
-  const [count, setCount] = useState(0); // 1st render
-
-  // Runs only when hasLiked changes
-  useEffect(() => {
-    console.log(`${title} has been liked: ${hasLiked}`);
-  }, [hasLiked]);
-
-  // Runs only once on the first render
-  useEffect(() => {
-    console.log(`CARD RENDERED`);
-  }, []);
-
-  return (
-    <div className="card" onClick={() => setCount((prev) => prev + 1)}>
-      <h2>
-        {title} <br /> {count}
-        {/* Conditionally rendered 1*/}
-        <br /> {count ? count : null}
-        {/* Conditionally rendered 2*/}
-        <br /> {count || null}
-      </h2>
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ? "<3" : "Like"}
-      </button>
-    </div>
-  );
-};
+import React from "react";
 
 const App = () => {
   return (
-    <div className="card-container">
-      <Card title="Movie 1" />
-      <Card title="Movie 2" />
-      <Card title="Movie 3" />
-    </div>
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <h1>Find Movies You'll Enjoy Without the Hassle</h1>
+        </header>
+      </div>
+    </main>
   );
 };
 
 export default App;
-
-// TIME 35min
