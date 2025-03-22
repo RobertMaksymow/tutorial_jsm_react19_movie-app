86b7947e5d0f8726e777305c4236702d4fda90cf
RobertMaksymow <114484269+RobertMaksymow@users.noreply.github.com>
2025-03-22 21:36:25 +0000

    Search trend logic implemented using AppWright for backend side. Time 1h54min.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { useDebounce } from "react-use";
 import Search from "./components/Search";
 import Spinner from "./components/Spinner";
 import MovieCard from "./components/MovieCard";
+import { updateSearchCount } from "./appwright";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -46,7 +47,12 @@ const App = () => {
         setMovieList([]);
         return;
       }
+
       setMovieList(data.results || []);
+
+      if (query && data.results.length > 0) {
+        updateSearchCount(query, data.results[0]);
+      }
     } catch (error) {
       console.error(`Error fetching movies: ${error}`);
       setErrorMessage(
